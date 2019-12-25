2f92f2fa2e00d5a0ff9c9b22313ecc58b1ac554a
AYIDouble <alpaypython@gmail.com>
2019-12-25 09:21:35 +0100

    ⬆️ Init

---
+++ helloworld.asm
@@ -0,0 +1,17 @@
+SECTION .data
+msg     db      'Hello World!', 0Ah
+ 
+SECTION .text
+global  _start
+ 
+_start:
+ 
+    mov     edx, 13
+    mov     ecx, msg
+    mov     ebx, 1
+    mov     eax, 4
+    int     80h
+ 
+    mov     ebx, 0      ; return 0 status on exit - 'No Errors'
+    mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
+    int     80h

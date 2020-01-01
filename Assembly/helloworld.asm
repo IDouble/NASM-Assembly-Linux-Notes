aa461b48f6773105c3c0e29d124e9398e7a95f2a
AYIDouble <alpaypython@gmail.com>
2020-01-01 17:15:07 +0100

    🔁 Edit Assembly

--- Assembly/helloworld.asm
+++ Assembly/helloworld.asm
@@ -1,3 +1,8 @@
+; Hello World Program - asmtutor.com
+; Compile with: nasm -f elf helloworld.asm
+; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 helloworld.o -o helloworld
+; Run with: ./helloworld
+ 
 SECTION .data
 msg     db      'Hello World!', 0Ah
  
